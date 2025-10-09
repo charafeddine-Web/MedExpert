f7f4203c14930b9422480758b205b585bd256825
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-10-09 19:25:10 +0100

    add generalist DAO

--- src/main/java/org/example/medexpert/dao/GénéralisteDAO.java
+++ src/main/java/org/example/medexpert/dao/GénéralisteDAO.java
@@ -1,4 +1,73 @@
 package org.example.medexpert.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.example.medexpert.model.Généraliste;
+import org.example.medexpert.util.JpaUtil;
+import java.util.List;
+
 public class GénéralisteDAO {
+
+    EntityManager em = JpaUtil.getEntityManager();
+
+    public void create(Généraliste generaliste) {
+        try {
+            em.getTransaction().begin();
+            em.persist(generaliste);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public Généraliste findById(Long id) {
+        try {
+            return em.find(Généraliste.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Généraliste> findAll() {
+        try {
+            TypedQuery<Généraliste> query = em.createQuery("SELECT g FROM Généraliste g", Généraliste.class);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void update(Généraliste generaliste) {
+        try {
+            em.getTransaction().begin();
+            em.merge(generaliste);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            em.getTransaction().begin();
+            Généraliste generaliste = em.find(Généraliste.class, id);
+            if (generaliste != null) {
+                em.remove(generaliste);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+
 }
