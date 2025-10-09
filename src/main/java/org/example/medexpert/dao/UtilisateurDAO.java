502c37c3c2a8f918a4b9168c8f0f9979e75d9dd6
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-10-09 17:10:37 +0100

    add Utilisateur Service

--- src/main/java/org/example/medexpert/dao/UtilisateurDAO.java
+++ src/main/java/org/example/medexpert/dao/UtilisateurDAO.java
@@ -1,4 +1,74 @@
 package org.example.medexpert.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import org.example.medexpert.model.Utilisateur;
+import org.example.medexpert.util.JpaUtil;
+
+import java.util.List;
+
 public class UtilisateurDAO {
+
+    private final EntityManager em = JpaUtil.getEntityManager();
+
+
+    public void save(Utilisateur utilisateur){
+        EntityTransaction tx= em.getTransaction();
+        try{
+            tx.begin();
+            em.persist(utilisateur);
+            tx.commit();
+        }catch (Exception e){
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+
+    public Utilisateur findById( long id){
+        return em.find(Utilisateur.class,id);
+    }
+
+    public List<Utilisateur> findAll(){
+        return em.createQuery("from Utilisateur ",Utilisateur.class).getResultList();
+    }
+
+
+    public void update(Utilisateur utilisateur){
+        EntityTransaction tx= em.getTransaction();
+        try{
+            tx.begin();
+            em.merge(utilisateur);
+            tx.commit();
+        }catch (Exception e){
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Long id) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Utilisateur utilisateur = em.find(Utilisateur.class, id);
+            if (utilisateur != null) {
+                em.remove(utilisateur);
+            }
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public Utilisateur findByEmail(String email) {
+        try {
+            return em.createQuery("SELECT u FROM Utilisateur u WHERE u.email = :email", Utilisateur.class)
+                    .setParameter("email", email)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
